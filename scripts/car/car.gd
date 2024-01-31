43aacb1d48961672a1deac6d392a474557db3b9e
Amon De Shir <amonsir29@gmail.com>
2024-01-31 01:31:49 +0100

    prevent stuck when flipped

--- scripts/car/car.gd
+++ scripts/car/car.gd
@@ -16,7 +16,7 @@ var engine_power: float
 @export
 var max_steering_angle: float = 30.0
 @export
-var steering_speed: float = 0.3
+var steering_speed: float = 10
 
 @export
 var debug: bool = false
@@ -25,7 +25,23 @@ var axel_input: float
 var steering_input: float
 var steering_angle: float
 
+func _ready():
+	max_steering_angle = deg_to_rad(max_steering_angle)
+
+
 func _process(_delta):
 	axel_input = Input.get_axis("backward", "forward")
 	steering_input = Input.get_axis("turn_right", "turn_left")
 	steering_angle = max_steering_angle * steering_input
+
+
+func _physics_process(delta):
+	unflip(delta)
+
+
+func unflip(delta: float):
+	if rotation.z < deg_to_rad(30):
+		return
+	
+	var rot_speed = lerpf(0.001, 0.01, rad_to_deg(rotation.z)/90) * mass * delta
+	rotation.z = lerp_angle(rotation.z, 0,  rot_speed)
